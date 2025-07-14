ed8bbdef644d386d706475050507133bfade8361
Mai <maidev@Mac.fibertel.com.ar>
2025-07-14 11:46:27 -0300

    contacto and team

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,43 +3,48 @@ import style from "../styles/Contacto.module.css"
 function Contacto() {
   return (
     <div className={style.container}>
-     <div className={style.containerInfo}>
-           <h2>CONTACTO</h2>
+
+
+      <div className={style.containerInformation}>
+        <div className={style.lineaBottom}></div>
+        <h2>CONTACTO</h2>
         <form action="">
-        <div className={style.inputContainer}>
-          <span className={style.placeholderFijo}>nombre y apellido:</span>
+          <div className={style.inputContainer}>
+            <span className={style.placeholderFijo}>nombre y apellido:</span>
             <input type="text" />
-        </div>
-           <div className={style.inputContainer}>
-          <span className={style.placeholderFijo}>email:</span>
+          </div>
+          <div className={style.inputContainer}>
+            <span className={style.placeholderFijo}>email:</span>
             <input type="text" />
-        </div>
-           <div className={style.inputContainer}>
-          <span className={style.placeholderFijo}>asunto:</span>
+          </div>
+          <div className={style.inputContainer}>
+            <span className={style.placeholderFijo}>asunto:</span>
             <input type="text" />
-        </div>
-           <div className={style.inputContainer}>
-          <span className={style.placeholderFijo}>rubro:</span>
+          </div>
+          <div className={style.inputContainer}>
+            <span className={style.placeholderFijo}>rubro:</span>
             <input type="text" />
-        </div>
-         
-      
-           <button className={style.btn}>Contactarme</button>
+          </div>
+
+
+          <button className={style.btn}>Contactarme</button>
 
 
         </form>
-        <div className={style.redes}>
-            <img src="" alt="instagram" />
-            <img src="" alt="linkedin" />
-            <img src="" alt="whatsapp" />
+       
+
+      </div>
+      <div className={style.containerInfo}> </div>
+       <div className={style.redes}>
+          <img src="./LinkedIn.png" alt="instagram" />
+          <img src="Instagram.png" alt="linkedin" />
+          <img src="WhatsApp.png" alt="whatsapp" />
         </div>
-    
-     </div>
-            <footer>
-            <p>Politica de privacidad</p>
-            <p>aviso legal</p>
-            <p>politica de cookies</p>
-        </footer>
+      <footer>
+        <p>Politica de privacidad</p>
+        <p>aviso legal</p>
+        <p>politica de cookies</p>
+      </footer>
     </div>
   )
 }
