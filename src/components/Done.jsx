98404f36a970eaf39a76536fa6c38852a1d77dc2
Mai <maidev@Mac.fibertel.com.ar>
2025-08-04 20:06:02 -0300

    call to action

--- src/components/Done.jsx
+++ src/components/Done.jsx
@@ -3,14 +3,14 @@ import style from "../styles/Done.module.css";
 
 
 function Done() {
- 
+
   const titleVariants = {
     hidden: {
-      clipPath: "inset(100% 0% 0% 0%)", 
+      clipPath: "inset(100% 0% 0% 0%)",
       opacity: 0,
     },
     visible: {
-      clipPath: "inset(0% 0% 0% 0%)", 
+      clipPath: "inset(0% 0% 0% 0%)",
       opacity: 1,
       transition: {
         duration: 2,
@@ -40,52 +40,63 @@ function Done() {
         </div>
         <p className={style.subtitle}><span className={style.span}>CODEX</span> featured projects</p>
         <div className={style.containerAllProyect}>
-        <div className={style.containerBoxOne}>
+          <div className={style.containerBoxOne}>
             <div className={style.containerProyect}>
-            <img className={style.img} src="/wifrut.png" alt="wifrut" />
-            <p className={style.nameProyect}> wifrut.com</p>
-            <p className={style.descriptionProyect}>Brand design, front- and back-end development, systems design.</p>
-           <a href="https://wifrut.com"> <img className={style.link} src="/Linking.png" alt="link" /></a>
-          </div>
-          <div className={style.containerProyect}>
-            <img  className={style.img}  src="crea.png" alt="crea" />
-            <p className={style.nameProyect}>creamarketingparavos.com</p>
-            <p className={style.descriptionProyect}>Brand design, front- and back-end development, systems design.</p>
-            <a href="https://creamarketingparavos.com"><img className={style.link} src="/Linking.png" alt="link" /></a>
-          </div>
-        </div>
-       <div className={style.containerBoxTwo}>
-           <div className={style.containerProyect}>
-            <img  className={style.img}  src="/growth.png" alt="growth" />
-            <p className={style.nameProyect}>Growth web app</p>
-            <p className={style.descriptionProyect}>Brand design, front- and back-end development, systems design.</p>
-               <img className={style.link} src="/Linking.png" alt="link" />
-          </div>
-          <div className={style.containerProyect}>
-            <img   className={style.img}  src="/orion.png" alt="orion" />
-            <p className={style.nameProyect}>Orion hairstudio</p>
-            <p className={style.descriptionProyect}>Brand identity</p>
-               <img  className={style.link} src="/Linking.png" alt="link" />
-          </div>
-       </div>
-        <div className={style.containerBoxTres}>
+              <img className={style.img} src="/wifrut.png" alt="wifrut" />
+              <p className={style.nameProyect}> wifrut.com</p>
+              <p className={style.descriptionProyect}>Brand design, front- and back-end development, systems design.</p>
+              <a href="https://wifrut.com"> <img className={style.link} src="/Linking.png" alt="link" /></a>
+            </div>
             <div className={style.containerProyect}>
-            <img className={style.app} src="/app.pn" alt="app" />
-            <p className={style.nameProyect}>Growth mobile app</p>
-            <p className={style.descriptionProyect}>Development and systems design.</p>
-               <img className={style.link} src="/Linking.png" alt="link" />
+              <img className={style.img} src="crea.png" alt="crea" />
+              <p className={style.nameProyect}>creamarketingparavos.com</p>
+              <p className={style.descriptionProyect}>Brand design, front- and back-end development, systems design.</p>
+              <a href="https://creamarketingparavos.com"><img className={style.link} src="/Linking.png" alt="link" /></a>
+            </div>
           </div>
-          <div className={style.containerProyect}>
-            <img  className={style.img}  src="portafolio.png" alt="portafolio" />
-            <p className={style.nameProyect}>Dev portfolio</p>
-            <p className={style.descriptionProyect}>Front design and development</p>
-             <a href="https://maidev.wtf"><img className={style.link} src="/Linking.png" alt="link" /></a>
+          <div className={style.containerBoxTwo}>
+            <div className={style.containerProyect}>
+              <img className={style.img} src="/growth.png" alt="growth" />
+              <p className={style.nameProyect}>Growth web app</p>
+              <p className={style.descriptionProyect}>Brand design, front- and back-end development, systems design.</p>
+              <img className={style.link} src="/Linking.png" alt="link" />
+            </div>
+            <div className={style.containerProyect}>
+              <img className={style.img} src="/orion.png" alt="orion" />
+              <p className={style.nameProyect}>Orion hairstudio</p>
+              <p className={style.descriptionProyect}>Brand identity</p>
+              <img className={style.link} src="/Linking.png" alt="link" />
+            </div>
           </div>
-        </div>
+          <div className={style.containerBoxTres}>
+            <div className={style.containerProyect}>
+              <img className={style.app} src="/app.pn" alt="app" />
+              <p className={style.nameProyect}>Growth mobile app</p>
+              <p className={style.descriptionProyect}>Development and systems design.</p>
+              <img className={style.link} src="/Linking.png" alt="link" />
+            </div>
+            <div className={style.containerProyect}>
+              <img className={style.img} src="portafolio.png" alt="portafolio" />
+              <p className={style.nameProyect}>Dev portfolio</p>
+              <p className={style.descriptionProyect}>Front design and development</p>
+              <a href="https://maidev.wtf"><img className={style.link} src="/Linking.png" alt="link" /></a>
+            </div>
+          </div>
         </div>
 
       </div>
-     <a className={style.textBottomLink} href="#contacto"> <p className={style.textBottom}>Contactanos para ver mas</p></a>
+  <div className={style.containerButtonWrapper}>
+        <div className={style.containerButton}>
+         <a href="https://wa.link/3425543308"><button className={style.button}>Let’s build it!</button></a>
+        <div className={style.containerText}>
+          <p>take</p>
+          <p>action and</p>
+          <p>boost</p>
+          <p>your</p>
+          <p>business</p>
+        </div>
+      </div>
+  </div>
     </div>
   );
 }
